5c6326f0783a7a13a054ea2a1b710e043c590a9b
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-01 22:00:38 +0500

    set favicon of this file

--- shopcart/src/main.jsx
+++ shopcart/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'swiper/css';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
