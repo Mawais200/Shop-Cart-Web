942fde70b26f497baca5b19826cf61c7cac1e7f7
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-06 19:47:05 +0500

    add a code in popup folder and in wwhich add add code or transition ease in out and download Aos in terminal

--- shopcart/src/App.jsx
+++ shopcart/src/App.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Hero from './Hero/hero'
 import Component from './Component/Navitems'
 import Products from './Products/products'
@@ -12,17 +15,31 @@ import './App.css'
 
 function App() {
   
+const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
     <>
       <Component/>
       <Hero/>
       <Products/>
-      <TopProducts/>
+      <TopProducts handleOrderPopup={handleOrderPopup}/>
       <Banneres/>
       <Subscribe/>
       <Testimonial/>
-      <Popup/>
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
 
       
     
